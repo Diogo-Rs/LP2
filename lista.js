8c795784145e76096a7caa8ac0b50c25665eb60d
Diogo-Rs <141764287+Diogo-Rs@users.noreply.github.com>
2024-10-02 17:23:24 -0400

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,26 @@
+//pushe 
+// let A = new Array();
+// A.push(2, 4, 5, 6, 7,8);
+// console.log(A);
+
+
+//Shift
+// let lanches = ["GAME OF FOME", "BACON BAD", "PRISON BACON", "VAMPIRE CHEESE"]
+// lanches.shift();
+// console.log(lanches);
+
+//Sort
+
+
+// let qtdLinhas = 5
+// let qtdColunas = 5
+// let matriz = new Array(qtdLinhas);
+// for (let l = 1; l < qtdLinhas; l++) {
+//     matriz[l] = new Array(qtdColunas);
+//     for (let c = 1; c < qtdColunas; c++) {
+//         matriz[l][c] = parseInt(Math.random() * 10);
+//     }
+// }
+
+// console.table(matriz);
+// console.table(matriz.sort());
